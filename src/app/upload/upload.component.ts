ad55d0654f0a7eb1dea9977030de18f1765c24fa
Alisa <alisa.barsky@gmail.com>
2017-07-24 22:07:51 +0300

    new changes

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -3,6 +3,8 @@ import {Video} from '../video/video.model';
 import {NgForm} from '@angular/forms';
  import { NgModel } from '@angular/forms';
  import {WrapperComponent} from '../wrapper/wrapper.component';
+// import {VideoService} from '../services/video.service';
+
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -17,10 +19,15 @@ export class UploadComponent implements OnInit {
  
   onSubmit(f :NgForm)
    { 
- f.value.url;
 
        this.video = new Video(0,f.value.video_name,f.value.url,f.value.concert_city,f.value.user
            ,f.value.band_name,f.value.song_name, f.value.concert_name, f.value.concert_date,0);
+
+  //     videoService.uploadVideo(this.video)
+   //    .then(
+
+/**/
+     //    );
 
     }
    
